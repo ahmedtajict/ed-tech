4466b44b4297488a5ff0898082abbc27a046d977
ahmedtajict <ahmedtajict@gmail.com>
2022-08-21 03:30:49 +0600

    courses added

--- src/Pages/Home/Course/Course.js
+++ src/Pages/Home/Course/Course.js
@@ -5,7 +5,19 @@ const Course = ({ course }) => {
 
     return (
         <div>
-            <h1>this is service name: {name}</h1>
+
+            <div className="card bg-base-200 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src={img} alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title font-bold text-3xl text-lime-700">{name}</h2>
+                    <p className='font-bold'>{description}</p>
+                    <div className="card-actions">
+                        <h2 className='text-2xl font-bold my-5 text-lime-700'>Price: {price}</h2><button className="btn bg-primary text-red-700 mx-2 my-3">Buy Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Courses/Courses.js
+++ src/Pages/Home/Courses/Courses.js
@@ -11,15 +11,18 @@ const Courses = () => {
             .then(data => setCourses(data));
     }, [])
     return (
-        <div>
-            <h1>Courses: {courses.length}</h1>
-            {
-                courses.map(course => <Course
-                    key={course.id}
-                    course={course}
-                >
-                </Course>)
-            }
+        <div className=''>
+            <h1 className='text-center text-5xl text-lime-700 font-bold my-8'>Courses</h1>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 my-8'>
+                {
+                    courses.map(course => <Course
+                        key={course.id}
+                        course={course}
+                    >
+                    </Course>)
+                }
+
+            </div>
 
         </div>
     );
